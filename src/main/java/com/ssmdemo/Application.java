959636f2613d858fbdc02fa7bf95927512e3713c
zhuguangchuanwj62961 <zhuguangchuanwj62961@touna.cn>
2018-05-29 16:58:22 +0800

    add druid and lombok log4j2

--- src/main/java/com/ssmdemo/Application.java
+++ src/main/java/com/ssmdemo/Application.java
@@ -1,10 +1,16 @@
 package com.ssmdemo;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 /**
  * Created by Administrator on 2016/11/25.
@@ -12,6 +18,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  */
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableTransactionManagement
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) throws Exception{
         SpringApplication.run(Application.class,args);
@@ -20,6 +27,12 @@ public class Application extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.druid")
+    public DataSource dataSourceOne(){
+        return DruidDataSourceBuilder.create().build();
     }
  }
 
